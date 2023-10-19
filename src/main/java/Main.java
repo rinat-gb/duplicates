c9a0ace827d405b4148fffc2868cae91a03f3996
Rinat <rinsdr@yandex.ru>
2023-10-19 20:12:41 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    private static List<Integer> findDuplicates(List<Integer> list) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        List<Integer> retList = list
+                .stream()
+                .filter(el -> uniqueNumbers.add(el))
+                .collect(Collectors.toList());
+        return retList;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 2, 4, 5, 3, 6, 6);
+        System.out.println(findDuplicates(list));
+    }
+}
